21e7843ed631b491dc61c211bd31c5f818a189a2
joshuaxchang <21186144+joshuaxchang@users.noreply.github.com>
2025-09-05 05:51:05 +0000

    force image processing at build time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
@@ -14,5 +12,11 @@ export default defineConfig({
       enabled: true,
     },
   }),
+  // This forces Astro to process images at build time instead of on-demand,
+  // which fixes the timeout errors on Cloudflare.
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
-
